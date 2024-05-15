2dbb8ddcabd153100ba480f5c9b45151149bd801
Jay <lelerukjaymoh@gmail.com>
2024-05-15 05:42:41 +0300

    project setup and connect wallet implementation

---
+++ src/config/index.tsx
@@ -0,0 +1,31 @@
+// config/index.tsx
+
+import { defaultWagmiConfig } from "@web3modal/wagmi/react/config";
+
+import { cookieStorage, createStorage } from "wagmi";
+import { mainnet, sepolia } from "wagmi/chains";
+
+// Your WalletConnect Cloud project ID
+export const projectId = process.env.NEXT_PUBLIC_WC_PROJECT_ID || "";
+
+// Create a metadata object
+const metadata = {
+  name: "Test",
+  description: "Web3Modal Example",
+  url: "http://localhost:3000", // origin must match your domain & subdomain
+  icons: ["https://avatars.githubusercontent.com/u/37784886"],
+};
+
+// Create wagmiConfig
+const chains = [mainnet, sepolia] as const;
+
+export const config = defaultWagmiConfig({
+  chains,
+  projectId,
+  metadata,
+  ssr: true,
+  storage: createStorage({
+    storage: cookieStorage,
+  }),
+  enableEmail: true, // Optional - false by default
+});
